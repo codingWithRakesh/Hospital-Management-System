dc15593f4a3ee396a49037159ee2a46d00f4dd9a
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-12-29 00:04:24 +0530

    Add security configuration with in-memory user details and password encoding

--- src/main/java/com/undefiend/hospitalManagement/config/ModdleMapperConfig.java
+++ src/main/java/com/undefiend/hospitalManagement/config/ModdleMapperConfig.java
@@ -3,6 +3,8 @@ package com.undefiend.hospitalManagement.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ModdleMapperConfig {
@@ -10,5 +12,10 @@ public class ModdleMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
